cd163d17e8de4f481b7ae1ea67734d7547394753
malie480 <165070798+malie480@users.noreply.github.com>
2024-04-05 09:35:53 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(5, 3), 9)
+        self.assertEqual(add(5, 3), 8)
 
 if __name__ == '__main__':
     unittest.main()
